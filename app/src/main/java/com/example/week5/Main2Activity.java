852be4177248359d96a219e3272dbcef54d6a848
ldy0309 <1055639197@qq.com>
2019-10-11 19:04:07 +0800

    屏幕方向控制

--- app/src/main/java/com/example/week5/Main2Activity.java
+++ app/src/main/java/com/example/week5/Main2Activity.java
@@ -16,24 +16,48 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void btn1(View v){
-        show(1);
+        show1(1);
     }
 
     public void btn2(View v){
-        show(2);
+        show1(2);
     }
 
     public void btn3(View v){
-        show(3);
+        show1(3);
     }
+
+    public void btn12(View v){
+        show2(1);
+    }
+
+    public void btn22(View v){
+        show2(2);
+    }
+
+    public void btn32(View v){
+        show2(3);
+    }
+
+
+
 
     public void btnReset(View v){
         TextView out = (TextView)findViewById(R.id.txt2);
+        TextView out2 = (TextView)findViewById(R.id.textView2);
         out.setText("0");
+        out2.setText("0");
     }
 
-    public void show(int i){
+    public void show1(int i){
         TextView out = (TextView)findViewById(R.id.txt2);
+        String oldScore = (String) out.getText();
+        String newScore = String.valueOf(Integer.parseInt(oldScore)+i);
+        out.setText(newScore);
+    }
+
+    public void show2(int i){
+        TextView out = (TextView)findViewById(R.id.textView2);
         String oldScore = (String) out.getText();
         String newScore = String.valueOf(Integer.parseInt(oldScore)+i);
         out.setText(newScore);
